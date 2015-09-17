0052a2c417a096edc6ce3eeb71eae41d68cc2b9c
Tomás Ortí Martín <tomasorti@gmail.com>
2015-09-17 11:55:49 +0200

    Add ping tester to check my local connection.

---
+++ bin/ping-tester.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true; do
+    #     *** DATE: Thu Sep 17 10:17:50 CEST 2015  ***
+    echo "\n*** DATE:" `date` " ***";
+    echo "********************************************"
+    ping -c5 $1;
+done

a6ac78369650a15bdef7a21091338ca8f922b7eb
Tomas <tomas@MacAir.local>
2015-09-20 20:58:58 +0200

    Modify echoed message for later parsing.

--- bin/ping-tester.bash
+++ bin/ping-tester.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 while true; do
-    #     *** DATE: Thu Sep 17 10:17:50 CEST 2015  ***
-    echo -e "\n*** DATE:" `date` " ***";
-    echo "********************************************"
+    #     *** DATE: Thu Sep 17 10:17:50 CEST 2015
+    echo -e "\n*** DATE:" `date`
+    echo "---------------------------------------"
     ping -c5 $1;
+    echo "###"
     sleep 1
 done

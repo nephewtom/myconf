a771f584a7ca2a4d5b521a78dda99f7ec22f7ba7
Tomás Ortí Martín <tomasorti@gmail.com>
2016-06-07 20:26:35 +0200

    Enablen cua-mode in term & ansi-term modes.

--- init.el
+++ init.el
@@ -735,6 +735,20 @@ by using nxml's indentation rules."
 ;; (add-to-list 'company-backends 'company-emoji)
 ;; (add-to-list 'load-path "~/.emacs.d/elpa/emoji-cheat-sheet-plus-20150617.631/emoji-cheat-sheet/")
 
+;; --- term stuff ---
+
+(defadvice term-line-mode (after term-line-mode-fixes ())
+  "Enable cua and transient mark modes in term-line-mode."
+  (set (make-local-variable 'cua-mode) t)
+  (set (make-local-variable 'transient-mark-mode) t))
+(ad-activate 'term-line-mode)
+
+(defadvice term-char-mode (after term-char-mode-fixes ())
+  "Disable cua and transient mark modes in term-char-mode."
+  (set (make-local-variable 'cua-mode) nil)
+  (set (make-local-variable 'transient-mark-mode) nil))
+(ad-activate 'term-char-mode)
+
 
 ;; --- Custom stuff
 (custom-set-faces

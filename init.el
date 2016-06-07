08487767df53337a9f642c6c2fdf2c85e54272bd
Tomás Ortí Martín <tomasorti@gmail.com>
2016-06-07 20:26:11 +0200

    Chez Scheme configuration.

--- init.el
+++ init.el
@@ -719,6 +719,17 @@ by using nxml's indentation rules."
                             ))
 
 
+;; --- Chez Scheme ---
+(autoload 'scheme-mode "iuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "iuscheme" "Switch to interactive Scheme buffer." t)
+(setq auto-mode-alist (cons '("\\.ss" . scheme-mode) auto-mode-alist))
+(autoload 'balanced-toggle "balanced" "Toggle balanced ``mode''" t)
+(autoload 'balanced-on "balanced" "Turn on balanced ``mode''" t)
+(add-hook 'scheme-mode-hook 'balanced-on)
+(custom-set-variables '(scheme-program-name "petite"))
+(setq scheme-program-name "petite")
+
+
 ;; --- Emoji stuff... on hold. I think I need unicode-fonts.
 ;; (require 'emoji-cheat-sheet-plus)
 ;; (add-to-list 'company-backends 'company-emoji)

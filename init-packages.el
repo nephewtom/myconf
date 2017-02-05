12bc2c90e9400a47638a0e0f4675f8f1ca2836f6
Tomás Ortí Martín <tomasorti@gmail.com>
2017-02-05 23:38:48 +0100

    Remove C/C++ part untill get an estable config.

--- init-packages.el
+++ init-packages.el
@@ -155,45 +155,11 @@
 
 (global-set-key (kbd "C-;") 'hippie-expand)
 
-;; --- Company, Irony, Clang, C++ stuff  ---
-;; NOTE: headers completion is BROKEN.
-;; TODO: try to go step-by-step in a init-cpp.el
+;; --- Company ---
 
 (require 'company)
 (setq company-global-modes '(not emacs-lisp-mode processing-mode text-mode))
 (global-set-key (kbd "M-y") 'company-complete)
-
-(add-hook 'c-mode-common-hook 'my-after-c-hook)
-(defun my-after-c-hook ()
-  "This is my hook after c-mode-common-hook."
-  (global-company-mode)
-  (setq company-backends (delete 'company-semantic company-backends))
-  (add-to-list 'company-backends 'company-clang)
-  (add-to-list 'company-backends 'company-c-headers)
-  (setq company-idle-delay 0)
-  (setq c-basic-offset 4)
-  )
-
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-
-;; --- C/C++ ---
-
-;; Switches between .h & .cpp files in C/C++
-(global-set-key (kbd "C-x C-o") 'ff-find-other-file)
-;; (global-set-key (kbd "<f4>") 'ff-find-other-file) ;; like QtCreator
-
-;; Disable AC mode in for c++
-(defadvice auto-complete-mode (around disable-auto-complete-for-c++)
-  "Disable AC (auto-complete mode) for c mode, as I use company."
-  (unless (or (eq major-mode 'c++-mode) (eq major-mode 'c-mode)) ad-do-it))
-(ad-activate 'auto-complete-mode)
-
-;; (require 'google-c-style) ;; TODO: Not tested
-;; (add-hook 'c-mode-common-hook 'google-set-c-style)
-
-;; Considering _ part of a word
-;; (modify-syntax-entry ?_ "w" c++-mode-syntax-table)
 
 (load "~/.emacs.d/dup-mode.el")
 

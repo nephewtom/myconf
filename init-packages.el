74165ccc8410da4afcf63138107aa304bed0b63c
Tomás Ortí Martín <tomasorti@gmail.com>
2017-04-26 00:50:36 +0200

    Fixed xah-lookup-drae & xah-look-linguee.

--- init-packages.el
+++ init-packages.el
@@ -447,23 +447,29 @@ by using nxml's indentation rules."
 (require 'xah-lookup)
 
 ;; M-x rae
-(defun xah-lookup-drae (&optional φword)
-  "Lookup definition of current ΦWORD or text selection in URL 'http://dle.rae.es/?w=�'."
+(defun xah-lookup-drae (&optional *word)
+  "Lookup definition of current *WORD or text selection in URL 'http://dle.rae.es/?w='."
   "Note: this is the old address `http://buscon.rae.es/drae/srv/search?val='."
   (interactive)
   (xah-lookup-word-on-internet
-   φword
-   "http://dle.rae.es/?w=�"
-   xah-lookup-dictionary-browser-function))
+   *word
+   (get 'xah-lookup-drae 'xah-lookup-url )
+   (get 'xah-lookup-drae 'xah-lookup-browser-function  )))
+
+(put 'xah-lookup-drae 'xah-lookup-url "http://dle.rae.es/?w=word02051")
+(put 'xah-lookup-drae 'xah-lookup-browser-function xah-lookup-browser-function)
 
 ;; M-x uee
-(defun xah-lookup-linguee (&optional φword)
-  "Lookup definition of current ΦWORD or text selection in URL `http://www.linguee.es/espanol-ingles/search?source=auto&query='."
+(defun xah-lookup-linguee (&optional *word)
+  "Lookup definition of current *WORD or text selection in URL `http://www.linguee.es/espanol-ingles/search?source=auto&query='."
   (interactive)
   (xah-lookup-word-on-internet
-   φword
-   "http://www.linguee.es/espanol-ingles/search?source=auto&query=�"
-   xah-lookup-dictionary-browser-function))
+   *word
+   (get 'xah-lookup-linguee 'xah-lookup-url )
+   (get 'xah-lookup-linguee 'xah-lookup-browser-function )))
+ 
+(put 'xah-lookup-linguee 'xah-lookup-url  "http://www.linguee.es/espanol-ingles/search?source=auto&query=word02051")
+(put 'xah-lookup-linguee 'xah-lookup-browser-function xah-lookup-browser-function)
 
 (defalias 'xlgoogle 'xah-lookup-google) ;; M-x xlg
 (defalias 'xlwikipedia 'xah-lookup-wikipedia) ;; M-x xlw

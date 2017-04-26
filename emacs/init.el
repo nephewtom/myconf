9ffc12997697fe22b250941e2a26023b4ca7e3cf
Tomás Ortí Martín <tomasorti@gmail.com>
2017-04-26 02:40:32 +0200

    Moved elisp file for Emacs configuration to emacs directory

---
+++ emacs/init.el
@@ -0,0 +1,10 @@
+;;; package --- Emacs init.el file
+;;; Commentary:
+
+;;; Code:
+
+(load "~/myconf/emacs/init-basic.el")
+(load "~/myconf/emacs/init-packages.el")
+(setq custom-file "~/myconf/emacs/custom.el")
+(load custom-file)
+;;; init.el ends here

d65e124f4356d3fe82a5e4880e066392f449627c
Tomás Ortí Martín <tomasorti@gmail.com>
2017-05-23 02:54:47 +0200

    init-cpp.el also loads custom variables.

--- emacs/init-cpp.el
+++ emacs/init-cpp.el
@@ -6,4 +6,6 @@
 (load "~/myconf/emacs/basic.el")
 (load "~/myconf/emacs/basic-packages.el")
 (load "~/myconf/emacs/cpp.el")
+(setq custom-file "~/myconf/emacs/custom.el")
+(load custom-file)
 ;;; init-basic-packages.el ends here

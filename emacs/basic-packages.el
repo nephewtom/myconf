d5cb591373982db9e3c8c3a2ddd01e5ce7101ef7
Tomás Ortí Martín <tomasorti@gmail.com>
2017-05-23 02:22:27 +0200

    Added some more packages.

--- emacs/basic-packages.el
+++ emacs/basic-packages.el
@@ -14,18 +14,30 @@
 (add-to-list 'rm-excluded-modes " MRev")
 (add-to-list 'rm-excluded-modes " ARev")
 
-(load "~/myconf/emacs/helm-n-buffers.el")
-(load "~/myconf/emacs/dired.el")
-
 ;; --- Move text ---
 ;; It allows you to move the current line using M-up / M-down
 ;; if a region is marked, it will move the region instead.
 (require 'move-text)
 (move-text-default-bindings)
 
+;; --- Git & Svn ---
+(require 'magit)
+(setq magit-last-seen-setup-instructions "1.4.0")
+(global-set-key (kbd "C-x g") 'magit-status)
+
 ;; --- Company ---
 (require 'company)
 (setq company-global-modes '(not processing-mode text-mode))
 (global-set-key (kbd "M-y") 'company-complete)
+
+(load "~/myconf/emacs/helm-n-buffers.el")
+(load "~/myconf/emacs/dired.el")
+
+(load "~/myconf/emacs/nxml.el")
+(load "~/myconf/emacs/hideshow.el")
+(load "~/myconf/emacs/xah-lookup.el")
+
+(load "~/myconf/emacs/markdown.el")
+(load "~/myconf/emacs/org-mode.el")
 
 ;;; init-basic-packages.el ends here

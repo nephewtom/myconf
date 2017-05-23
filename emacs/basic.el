f5d32959c0f4ca961154c86181d03935a50f5620
Tomás Ortí Martín <tomasorti@gmail.com>
2017-05-23 23:08:06 +0200

    Added defalias file for fast M-x aliases.

--- emacs/basic.el
+++ emacs/basic.el
@@ -5,6 +5,7 @@
 (load "~/myconf/emacs/duplicate-line.el")
 (load "~/myconf/emacs/xah-cut-copy.el")
 (load "~/myconf/emacs/keybindings.el")
+(load "~/myconf/emacs/defalias.el")
 (load "~/myconf/emacs/term.el")
 (load "~/myconf/emacs/compilation.el")
 (load "~/myconf/emacs/elisp.el")
---
+++ emacs/defalias.el
@@ -0,0 +1,8 @@
+;; defalias for Fast M-x 
+(defalias 'hgrep 'helm-grep-do-git-grep)
+(defalias 'hman 'helm-man-woman)
+(defalias 'hfind 'helm-find)
+
+(defalias 'qr 'query-replace)
+(defalias 'qrr 'query-replace-regexp)
+

600c161274901d8c2c29e38d400eec48ef7ca261
Tomás Ortí Martín <tomasorti@gmail.com>
2017-05-23 02:23:01 +0200

    Added term, compilation and find elisp stuff.

--- emacs/basic.el
+++ emacs/basic.el
@@ -5,11 +5,24 @@
 (load "~/myconf/emacs/duplicate-line.el")
 (load "~/myconf/emacs/xah-cut-copy.el")
 (load "~/myconf/emacs/keybindings.el")
+(load "~/myconf/emacs/term.el")
+(load "~/myconf/emacs/compilation.el")
 
 ;; --- Elisp related
 (require 'hl-defined)
 (add-hook 'emacs-lisp-mode-hook 'hdefd-highlight-mode 'APPEND)
+
+;; http://emacsredux.com/blog/2014/06/18/quickly-find-emacs-lisp-sources/
+(define-key 'help-command (kbd "C-l") 'find-library)
+(define-key 'help-command (kbd "C-f") 'find-function)
+(define-key 'help-command (kbd "C-k") 'find-function-on-key)
+(define-key 'help-command (kbd "C-v") 'find-variable)
+
+;; Disable checkdoc in elisp
+(with-eval-after-load 'flycheck
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
 (package-initialize)
 
-
 ;;; basic.el ends here
+

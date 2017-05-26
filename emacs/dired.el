507603ba2a0e3a953670228a978f92af4c76783b
Tomás Ortí Martín <tomasorti@gmail.com>
2017-05-26 09:39:35 +0200

    Using dired+ now.

--- emacs/dired.el
+++ emacs/dired.el
@@ -2,13 +2,14 @@
 ;; TODO: Sort dired by time date as default 
 ;; https://superuser.com/questions/875241/emacs-dired-sorting-by-time-date-as-default
 
-(require 'dired )
+(setq diredp-hide-details-initially-flag nil)
+(require 'dired+)
 (setq dired-listing-switches "-lkt")
 ;; move to up directory with '.'
 (define-key dired-mode-map (kbd ".") (lambda () (interactive) (find-alternate-file "..")))
 ;; Following key is already binded to 'a'
 (define-key dired-mode-map (kbd "f") 'dired-find-alternate-file)
-
+(define-key dired-mode-map (kbd "M-p") 'cua-scroll-down)
 ;; Auto-refresh dired on file change
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 (setq dired-auto-revert-buffer t)

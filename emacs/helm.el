cfe328d5c644610b258cf9883ee26fc26a442104
Tomás Ortí Martín <tomasorti@gmail.com>
2017-07-19 02:12:32 +0200

    Changed helm keybindings when inside helm.

--- emacs/helm.el
+++ emacs/helm.el
@@ -17,3 +17,16 @@
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "M-x")  'helm-select-action) ; list actions using M-x again
+
+(setq helm-split-window-in-side-p nil)
+
+
+
+
+(define-key helm-map (kbd "M-n")  'helm-next-page)
+(define-key helm-map (kbd "M-p")  'helm-previous-page)
+(define-key helm-map (kbd "C-v")  'helm-yank-text-at-point)
+(define-key helm-map (kbd "C-<up>")  'previous-history-element)
+(define-key helm-map (kbd "C-<down>")  'next-history-element)
+
+

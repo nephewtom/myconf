56825975522410133f9e4484efcece6fa72b8834
Tomás Ortí Martín <tomasorti@gmail.com>
2017-11-04 20:48:17 +0100

    python.el is now loaded

--- emacs/python.el
+++ emacs/python.el
@@ -1,48 +1,12 @@
-;; init.el --- Emacs configuration
-
-;; INSTALL PACKAGES
-;; --------------------------------------
-
-;; From
-;; https://realpython.com/blog/python/emacs-the-best-python-editor/
-
-(require 'package)
-
-(add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar myPackages
-  '(better-defaults
-    ein
-    elpy
-    flycheck
-    material-theme
-    py-autopep8))
-
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
-
 (elpy-enable)
+
+;; What about these two if I use virtualenv ?
+(elpy-use-ipython "ipython3")
+(setq elpy-rpc-python-command "python3")
+
+(setq elpy-rpc-backend "jedi")
 
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
-
-(setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
-(global-linum-mode t) ;; enable line numbers globally
-
-;; init.el ends here

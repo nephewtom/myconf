31a5dbd95758fc9ad2470e10ef1fc7944d8beecd
Tomás Ortí Martín <tomasorti@gmail.com>
2017-11-22 00:41:29 +0100

    Added function to diff files from dired and to find-dired by date and size.

--- emacs/dired.el
+++ emacs/dired.el
@@ -3,15 +3,48 @@
 ;; https://superuser.com/questions/875241/emacs-dired-sorting-by-time-date-as-default
 
 (setq diredp-hide-details-initially-flag nil)
+
 (require 'dired+)
 (setq dired-listing-switches "-lkt")
+
 ;; move to up directory with '.'
 (define-key dired-mode-map (kbd ".") (lambda () (interactive) (find-alternate-file "..")))
+
 ;; Following key is already binded to 'a'
 (define-key dired-mode-map (kbd "f") 'dired-find-alternate-file)
 (define-key dired-mode-map (kbd "M-p") 'backward-paragraph)
+(define-key dired-mode-map "e" 'ora-ediff-files)
+(define-key dired-mode-map "F" 'find-name-dired)
+
 ;; Auto-refresh dired on file change
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 (setq dired-auto-revert-buffer t)
 
 (put 'dired-find-alternate-file 'disabled nil)
+
+;; Esta funcions usa dired-jump para abrir en dired el buffer actual
+(defun open-in-dired ()
+  "Show current buffer on dired."
+  (interactive)
+  (if (buffer-file-name)
+      (dired-jump (buffer-file-name))
+    (message "This buffer is not a file in the filesystem.")))
+
+(setq find-args "")
+(defun find-dired-by-date (dir args)
+  (interactive (list (read-directory-name "Run find in directory: " nil "" t)
+		     (read-string "Run find (with args): " find-args
+				  '(find-args-history . 1))))
+  ;; Set to this value in order to get a find sorted by date
+  (setq find-ls-option '("-exec ls -lt {} + | cut -d ' ' -f5-" . "-lt"))
+  (find-dired dir args)
+  (setq find-ls-option '("-ls" . "-dilsb")))
+
+(defun find-dired-by-size (dir args)
+  (interactive (list (read-directory-name "Run find in directory: " nil "" t)
+		     (read-string "Run find (with args): " find-args
+				  '(find-args-history . 1))))
+  ;; Set to this one to get it sorted by size
+  (setq find-ls-option '("-exec ls -lSr {} + | cut -d ' ' -f5-" . "-lSr"))
+  (find-dired dir args)
+  (setq find-ls-option '("-ls" . "-dilsb")))
--- emacs/ediff.el
+++ emacs/ediff.el
@@ -1,4 +1,25 @@
-;; Setting up Ediff
+;; https://oremacs.com/2017/03/18/dired-ediff/
+;; -*- lexical-binding: t -*-
+(defun ora-ediff-files ()
+  (interactive)
+  (let ((files (dired-get-marked-files))
+        (wnd (current-window-configuration)))
+    (if (<= (length files) 2)
+        (let ((file1 (car files))
+              (file2 (if (cdr files)
+                         (cadr files)
+                       (read-file-name
+                        "file: "
+                        (dired-dwim-target-directory)))))
+          (if (file-newer-than-file-p file1 file2)
+              (ediff-files file2 file1)
+            (ediff-files file1 file2))
+          (add-hook 'ediff-after-quit-hook-internal
+                    (lambda ()
+                      (setq ediff-after-quit-hook-internal nil)
+                      (set-window-configuration wnd))))
+      (error "no more than 2 files should be marked"))))
+
 ;; From: https://oremacs.com/2015/01/17/setting-up-ediff/
 (defmacro csetq (variable value)
   `(funcall (or (get ',variable 'custom-set)

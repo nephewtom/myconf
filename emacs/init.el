51a457f9f01268061787c7f08aea53220d221abc
Tomás Ortí Martín <tomasorti@gmail.com>
2017-12-08 20:19:19 +0100

    Updated move files on init.el and added log.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,6 @@
+;; Provide timestamp to *Messages* logs
+(load "~/myconf/emacs/log.el")
+
 ;; Try to speed up start-up
 (setq-default gc-cons-threshold (* 100 1024 1024))
 ;;(let ((file-name-handler-alist nil)) "~/myconf/emacs/init.el")
@@ -32,24 +35,25 @@
 (load "~/myconf/emacs/sudo.el")
 (load "~/myconf/emacs/defalias.el")
 
-(load "~/myconf/emacs/use/dired.el")
+(load "~/myconf/emacs/dired.el")
 (load "~/myconf/emacs/ediff.el")
-(load "~/myconf/emacs/use/helm.el")
-(load "~/myconf/emacs/use/movement.el")
-(load "~/myconf/emacs/use/buffers-utils.el")
-(load "~/myconf/emacs/use/elisp.el")
-(load "~/myconf/emacs/use/flycheck.el")
-(load "~/myconf/emacs/use/company.el")
-(load "~/myconf/emacs/use/xah-lookup.el")
-(load "~/myconf/emacs/use/magit.el")
+(load "~/myconf/emacs/helm.el")
+(load "~/myconf/emacs/movement.el")
+(load "~/myconf/emacs/buffers-utils.el")
+(load "~/myconf/emacs/elisp.el")
+(load "~/myconf/emacs/flycheck.el")
+(load "~/myconf/emacs/company.el")
+(load "~/myconf/emacs/xah-lookup.el")
+(load "~/myconf/emacs/magit.el")
 
-(load "~/myconf/emacs/use/term.el")
-(load "~/myconf/emacs/use/org-mode.el")
-(load "~/myconf/emacs/use/nxml.el")
-(load "~/myconf/emacs/use/hideshow.el")
-(load "~/myconf/emacs/use/edit-with-emacs.el")
-(load "~/myconf/emacs/use/markdown.el")
-(load "~/myconf/emacs/use/python.el")
+(load "~/myconf/emacs/term.el")
+(load "~/myconf/emacs/org-mode.el")
+(load "~/myconf/emacs/nxml.el")
+(load "~/myconf/emacs/hideshow.el")
+(load "~/myconf/emacs/edit-with-emacs.el")
+(load "~/myconf/emacs/markdown.el")
+(load "~/myconf/emacs/python.el")
+(load "~/myconf/emacs/cpp.el")
 
 (load "~/myconf/emacs/keybindings.el") ;; has (require 'iso-transl)
 
@@ -57,8 +61,8 @@
 (load custom-file)
 (put 'scroll-left 'disabled nil)
 
-;;(load "~/myconf/emacs/use/smart-line.el")
-;;(load "~/myconf/emacs/use/spaceline.el")
+;;(load "~/myconf/emacs/smart-line.el")
+;;(load "~/myconf/emacs/spaceline.el")
 
 (server-start) ;; emacs server
 (message "Emacs ready with init.el !")

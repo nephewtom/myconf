366e75833db0a2ddaaa831d726ca808af752b0ed
Tomás Ortí Martín <tomasorti@gmail.com>
2019-01-26 18:45:59 +0100

    Update linux alias & env, profile emacs.

--- profile-dotemacs.bash
+++ profile-dotemacs.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # https://oremacs.com/2015/02/24/emacs-speed-test/
 
+PROFILE_FILE="~/myconf/emacs/profile-dotemacs.el"
 INIT_FILE="~/myconf/emacs/init.el"
 if [ -z "$1" ]; then
     echo "No init file provided, taking" $INIT_FILE
@@ -9,7 +10,7 @@ else
     echo "Emacs init file"
 fi
 
-emacs -Q -l ~/myconf/emacs/profile-dotemacs.el \
-      --eval "(setq profile-dotemacs-file \
-(setq load-file-name \"$INIT_FILE\"))" \
-      -f profile-dotemacs
+emacs -Q -l $PROFILE_FILE \
+          --eval "(setq profile-dotemacs-file \
+                        (setq load-file-name \"$INIT_FILE\"))" \
+          -f profile-dotemacs

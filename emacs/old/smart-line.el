11415ad7810679dd6865c179db6d2d5c9a692891
Tomás Ortí Martín <tomasorti@gmail.com>
2019-07-25 20:20:39 +0200

    Removed old directory

--- emacs/old/smart-line.el
+++
@@ -1,7 +0,0 @@
-;; --- Smart line ---
-;; https://github.com/Malabarba/smart-mode-line
-(setq sml/no-confirm-load-theme t)
-(setq sml/theme 'light)
-(sml/setup)
-(add-to-list 'rm-excluded-modes " MRev")
-(add-to-list 'rm-excluded-modes " ARev")

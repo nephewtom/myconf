dd6a2498a4f776230604f80bf059a8a5bab9a0ad
Tomás Ortí Martín <tomasorti@gmail.com>
2019-07-25 23:12:28 +0200

    Changed colors (faces) for region and hl-line

--- emacs/column-and-line-numbers.el
+++ emacs/column-and-line-numbers.el
@@ -2,12 +2,16 @@
 (column-number-mode t)
 (global-linum-mode t) ;; line numbers in all buffers
 
-;; https://stackoverflow.com/questions/9990370/how-to-disable-hl-line-feature-in-specified-mode
+;; https://stackoverflow.com/q/9990370/316232
 (global-hl-line-mode t) ;; highlight current line
-;;(global-hl-line-mode 0) ;; highlight current line
 (make-variable-buffer-local 'global-hl-line-mode)
 
-(set-face-background hl-line-face "#FFCA00") ;; orangewish color
-;;(set-face-background hl-line-face "#fbffab") ;; Test it here
-(set-face-attribute 'fringe nil :background "#d0f0ff") ;; Color blue en sangrado
-;;(set-face-attribute 'fringe nil :background "gray30") ;; Test it here
+;; current line highlighted color
+(set-face-background hl-line-face "#404040")
+
+;; region highlight color
+(set-face-attribute 'region nil :background "#848000") ;;
+
+;; fringe color (between line numbers and buffer)
+(set-face-attribute 'fringe nil :background "#505050")
+

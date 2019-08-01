73f275a6960571372491f328b8bf8033990f4487
Tomás Ortí Martín <tomasorti@gmail.com>
2019-08-01 23:19:59 +0200

    Added useful debug-emacs.el file

---
+++ emacs/debug-emacs.el
@@ -0,0 +1,14 @@
+(setq package-enable-at-startup nil)
+(package-initialize)
+(setq sml/no-confirm-load-theme t)
+
+(global-set-key [C-tab] 'other-window)
+;; Uncomment to make Emacs crash
+;;(sml/setup)
+;;(gterm)
+
+
+;; Disable all version control
+(setq vc-handled-backends nil)
+
+(color-theme-monokai)

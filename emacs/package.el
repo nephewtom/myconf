3d1f6596c948a58779fefed15c0c142334a00684
Tomás Ortí Martín <tomasorti@gmail.com>
2020-05-09 12:35:28 +0200

    Added missing package.el

---
+++ emacs/package.el
@@ -0,0 +1,31 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; list the packages you want
+(setq package-list '(use-package diminish edit-server buffer-move monokai-theme))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package diminish
+  :ensure t)
+
+(use-package bind-key
+  :ensure t)
+
+; set the path for manually installed packages
+(add-to-list 'load-path "~/.emacs.d/packages")

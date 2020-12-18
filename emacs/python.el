3bb0bd692d7aab22d2dc4fb60c0589bc9748abe4
Tomás Ortí Martín <tomasorti@gmail.com>
2020-12-18 17:38:04 +0100

    Set ipython3 as interpreter and python3 for flycheck executables

--- emacs/python.el
+++ emacs/python.el
@@ -1,19 +1,22 @@
 (use-package python
   :mode ("\\.py\\'" . python-mode)
   :bind (:map  python-mode-map
-         ("C->" . python-indent-shift-right)
-         ("C-<" . python-indent-shift-left))
+               ("C->" . python-indent-shift-right)
+               ("C-<" . python-indent-shift-left))
   :config
   (elpy-enable)
   ;; What about these two if I use virtualenv ?
-  (setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
+  (setq python-shell-interpreter "ipython3"
+        python-shell-interpreter-args "-i --simple-prompt")
 
   (setq elpy-rpc-python-command "python3")  
   (setq elpy-rpc-backend "jedi") ;; Do I have to setq this?
 
   (when (require 'flycheck nil t)
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (setq flycheck-python-flake8-executable "python3")
+    (setq flycheck-python-pycompile-executable "python3")
+    (setq flycheck-python-pylint-executable "python3")
     (add-hook 'elpy-mode-hook 'flycheck-mode)))
 
 ;; I think jedi uses autocomplete-mode (AC) and not company-mode

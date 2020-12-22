6b00d1786bd999b290c86725e66aafff153e6b85
Tomas Orti <tomas.orti.martin@ericsson.com>
2020-12-22 20:22:24 +0100

    Visual Studio environment vars

--- bin/vc.bat
+++ bin/vc.bat
@@ -1,4 +1,50 @@
-"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build//vcvars64.bat"
+set CommandPromptType=Native
+set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\
 
-REM "c:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/Tools/VsDevCmd.bat"
-REM "c:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/Tools/VsMSBuildCmd.bat"
+set ExtensionSdkDir=C:\Program Files (x86)\Microsoft SDKs\Windows Kits\10\ExtensionSDKs
+set Framework40Version=v4.0
+set FrameworkDir=C:\Windows\Microsoft.NET\Framework64\
+set FrameworkDir64=C:\Windows\Microsoft.NET\Framework64\
+set FrameworkVersion=v4.0.30319
+set FrameworkVersion64=v4.0.30319
+
+set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\ATLMFC\include;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\include;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7.2\include\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\ucrt;C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\shared;C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\um;C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\winrt;C:\Program Files (x86)\Windows Kits\10\include\10.0.18362.0\cppwinrt
+set LIB=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\ATLMFC\lib\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\lib\x64;C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7.2\lib\um\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.18362.0\um\x64;
+set LIBPATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\ATLMFC\lib\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\lib\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\lib\x86\store\references;C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.18362.0;C:\Program Files (x86)\Windows Kits\10\References\10.0.18362.0;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;
+
+set NETFXSDKDir=C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7.2\
+
+set Platform=x64
+
+set PROMPT=CLINK $P$G
+
+set UCRTVersion=10.0.18362.0
+set UniversalCRTSdkDir=C:\Program Files (x86)\Windows Kits\10\
+
+set VCIDEInstallDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\VC\
+set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\
+set VCToolsInstallDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\
+set VCToolsRedistDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Redist\MSVC\14.22.27821\
+set VCToolsVersion=14.22.27905
+set VisualStudioVersion=16.0
+set VS160COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\
+set VSCMD_ARG_app_plat=Desktop
+set VSCMD_ARG_HOST_ARCH=x64
+set VSCMD_ARG_TGT_ARCH=x64
+set VSCMD_VER=16.2.0
+set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\
+
+set WindowsLibPath=C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.18362.0;C:\Program Files (x86)\Windows Kits\10\References\10.0.18362.0
+set WindowsSdkBinPath=C:\Program Files (x86)\Windows Kits\10\bin\
+set WindowsSdkDir=C:\Program Files (x86)\Windows Kits\10\
+set WindowsSDKLibVersion=10.0.18362.0\
+set WindowsSdkVerBinPath=C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\
+set WindowsSDKVersion=10.0.18362.0\
+set WindowsSDK_ExecutablePath_x64=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools\x64\
+set WindowsSDK_ExecutablePath_x86=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools\
+
+set __DOTNET_ADD_64BIT=1
+set __DOTNET_PREFERRED_BITNESS=64
+set __VSCMD_PREINIT_PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Users\tomas\scoop\apps\python27\current\scripts;C:\Users\tomas\scoop\apps\python\current\Scripts;C:\Users\tomas\scoop\apps\maven\current\bin;C:\Users\tomas\scoop\shims;C:\Users\tomas\AppData\Local\Microsoft\WindowsApps;C:\Users\tomas\scoop\apps\git\current\usr\bin;C:\Users\tomas\bin;C:\4coder;C:\Users\tomas\myconf\bin;C:\Users\tomas\AppData\Local\Microsoft\WindowsApps
+
+set Path=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.22.27905\bin\HostX64\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\VC\VCPackages;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\bin\Roslyn;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Performance Tools\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Team Tools\Performance Tools;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Common\VSPerfCollectionTools\vs2019\\x64;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Common\VSPerfCollectionTools\vs2019\;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools\x64\;C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64;C:\Program Files (x86)\Windows Kits\10\bin\x64;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\\MSBuild\Current\Bin;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Users\tomas\scoop\apps\python27\current\scripts;C:\Users\tomas\scoop\apps\python\current\Scripts;C:\Users\tomas\scoop\apps\maven\current\bin;C:\Users\tomas\scoop\shims;C:\Users\tomas\AppData\Local\Microsoft\WindowsApps;C:\Users\tomas\scoop\apps\git\current\usr\bin;C:\Users\tomas\bin;C:\4coder;C:\Users\tomas\myconf\bin;C:\Users\tomas\AppData\Local\Microsoft\WindowsApps;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;c:/4coder-4.1.6/

009940c0f8a76326f928cc10088d4ba3cc718f21
Tomas Orti <tomas.orti.martin@ericsson.com>
2021-02-18 21:12:51 +0100

    Tested xlaunch on Ubuntu with WSL2

--- bin/start-xlaunch.sh
+++ bin/start-xlaunch.sh
@@ -2,7 +2,8 @@ echo "Check if vcxsrv is running..."
 cmd.exe /c tasklist.exe /FI "IMAGENAME eq vcxsrv.exe" | grep vcxsrv
 if [[ $? -ne 0 ]]; then
     echo "X11 Server not found. Starting VcXsrv now..."
-    xlaunch.exe -run ../config.xlaunch
+    cd ~/myconf/bin
+    xlaunch.exe -run config.xlaunch
     # vcxsrv.exe :0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl -dpi auto
 else
     echo "X11 Server already running!"
